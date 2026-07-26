6e28f68cc5c44a58bad6d5d2a27741afa5fa71f2
Ibrohim AJS <ibrohimsadzik@gmail.com>
2026-07-27 01:59:43 +0800

    Ibrohim-06: use shared ActuationCommand contract

--- services/actuation/app.py
+++ services/actuation/app.py
@@ -2,6 +2,8 @@ import json
 import zmq
 import paho.mqtt.client as mqtt
 
+from services.shared.schemas import ActuationCommand
+
 # MQTT setup
 client = mqtt.Client()
 client.connect("localhost", 1883, 60)
@@ -17,11 +19,18 @@ while True:
 
     message = socket.recv_json()
 
+    command = ActuationCommand(
+        joints=message["joints"]
+    )
+
     print(f"Received frame {message['frame_id']}")
 
     client.publish(
         "robot/joints",
-        json.dumps(message)
+        json.dumps({
+            "frame_id": message["frame_id"],
+            "joints": command.joints
+        })
     )
 
     print(
